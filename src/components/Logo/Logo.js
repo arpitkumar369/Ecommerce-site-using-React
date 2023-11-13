1c3974c03cd80cfbcbb150613d61fff3ba8f0e07
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2023-11-13 13:27:09 +0530

    Create Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Aroma from "../../assets/img/logo.png";
+
+
+
+const Logo = (props) => {
+
+  return (
+
+    <div onClick={props.click}>
+
+      <img src={Aroma} alt="logo"/>
+
+    </div>
+
+  )
+
+}
+
+
+
+export default Logo
